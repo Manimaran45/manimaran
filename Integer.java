e793c05724404a8730938e1eec21e60c0f6f155a
Manimaran45 <manimaran2014007@gmail.com>
2017-07-05 11:26:04 +0530

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,14 @@
+package maniya;
+
+import java.util.Scanner;
+
+public class Integer {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int a = java.lang.Integer.parseInt(str);
+		System.out.println(a);
+	}
+	}
+
+	
